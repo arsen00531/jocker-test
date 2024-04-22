a15edc00d945fcd174d3ed4b37a9e8d51668d974
SecondNiceee <col1596321@gmail.com>
2024-04-22 04:10:04 +0300

    dasda

--- src/pages/AdCreatingOne/Categories/Categories.jsx
+++ src/pages/AdCreatingOne/Categories/Categories.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import cl from './Categories.module.css'
 import upDown from '../../../images/icons/UpDown.svg'
 const Categories = ({className , taskInformation, setCatagoryChoiceOpen , setSubcategoryChoiceOpen }) => {
+    function format(arg){
+        let str = arg.split(' ')
+        let rezult = ''
+        for (let word of str){
+            if (rezult.length < 12){
+                rezult+=word + ' '
+            }
+            else{
+                break
+            }
+        }
+        rezult = rezult.slice(0 , rezult.length - 1)
+        if(rezult[rezult.length-1] === ','){
+            rezult = rezult.slice(0 , rezult.length - 1)
+        }
+        return(rezult)
+    }
     return (
         <div className = { className ? [cl.Categories , className].join(' ') : cl.Categories   }>
             <div className={cl.Categories__block}>
@@ -11,7 +28,7 @@ const Categories = ({className , taskInformation, setCatagoryChoiceOpen , setSub
             <hr className={cl.line} />
             <div className={cl.Categories__block}>
                 <p>Подкатегория</p>
-                <p  onClick={() => {setSubcategoryChoiceOpen(true)}} className={cl.Category__link} href="">{taskInformation.subCategory.slice(0,18)}
+                <p  onClick={() => {setSubcategoryChoiceOpen(true)}} className={cl.Category__link} href="">{format(taskInformation.subCategory)}..      
                 {/* {taskInformation.subCategory=== 'Выбрать' ? '' : '.'} */}
                 </p>
             </div>
