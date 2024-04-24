5d76e329dd2421c06f62728f208c49fcd7e8e3ca
SecondNiceee <col1596321@gmail.com>
2024-04-24 16:51:36 +0300

    dasdas

--- src/constants/Name.jsx
+++ src/constants/Name.jsx
@@ -1,11 +1,11 @@
-// const userInfo = {first : window.Telegram.WebApp.initDataUnsafe.user.first_name, 
-// second : window.Telegram.WebApp.initDataUnsafe.user.second_name,
-// photo : window.Telegram.WebApp.initDataUnsafe.user.photo_url}
+const userInfo = {first : window.Telegram.WebApp.initDataUnsafe.user.first_name, 
+second : window.Telegram.WebApp.initDataUnsafe.user.second_name,
+photo : window.Telegram.WebApp.initDataUnsafe.user.photo_url}
 
-const userInfo = { 
-    first : 'Перезапусти бота!!', 
-    second : '',
-    photo : 'Перезапусти бота!!'
+// const userInfo = { 
+//     first : 'Перезапусти бота!!', 
+//     second : '',
+//     photo : 'Перезапусти бота!!'
 
-  }
-export default userInfo;
+//   }
+// export default userInfo;
