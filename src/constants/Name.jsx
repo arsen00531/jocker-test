4ed0123c176074f6c0c51d7608c3cfe5dadc3144
SecondNiceee <col1596321@gmail.com>
2024-04-24 17:05:44 +0300

    dasda

--- src/constants/Name.jsx
+++ src/constants/Name.jsx
@@ -2,6 +2,7 @@ const userInfo = {first : window.Telegram.WebApp.initDataUnsafe.user.first_name,
 second : window.Telegram.WebApp.initDataUnsafe.user.second_name,
 photo : window.Telegram.WebApp.initDataUnsafe.user.photo_url}
 
+export default userInfo;
 // const userInfo = { 
 //     first : 'Перезапусти бота!!', 
 //     second : '',
